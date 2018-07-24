f18971746a67c64b86009d5bc5247ecff8b6d645
Arosos <wierzba96@o2.pl>
2018-07-24 13:55:54 +0200

    Post controller with Create action.

---
+++ SimpleWebApp/Controllers/PostsController.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SimpleWebApp.Areas.Identity.Data;
+using SimpleWebApp.Models;
+
+namespace SimpleWebApp.Controllers
+{
+    public class PostsController : Controller
+    {
+        private SimpleWebAppContext _conext;
+        private readonly UserManager<SimpleWebAppUser> _userManager;
+
+        public PostsController(SimpleWebAppContext context, UserManager<SimpleWebAppUser> userManager)
+        {
+            _conext = context;
+            _userManager = userManager;
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind(include:"Content")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                SimpleWebAppUser user = _userManager.FindByNameAsync(User.Identity.Name).Result;
+                post.Author = user;
+                post.AuthorID = user.Id;
+                _conext.Add(post);
+                await _conext.SaveChangesAsync();
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
