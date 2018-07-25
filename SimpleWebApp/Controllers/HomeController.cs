e8b104853faa88a70f1e90c70527d16aa945b07f
Arosos <wierzba96@o2.pl>
2018-07-25 12:20:24 +0200

    HomeController now returns all posts while user is signed in.

--- SimpleWebApp/Controllers/HomeController.cs
+++ SimpleWebApp/Controllers/HomeController.cs
@@ -4,15 +4,38 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SimpleWebApp.Areas.Identity.Data;
 using SimpleWebApp.Models;
+using SimpleWebApp.ViewModels;
 
 namespace SimpleWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private SimpleWebAppContext _context;
+        private readonly UserManager<SimpleWebAppUser> _userManager;
+
+        public HomeController(SimpleWebAppContext context, UserManager<SimpleWebAppUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                var posts = from p in _context.Posts
+                            orderby p.Timestamp descending
+                            select p;
+                HomeViewModel homeViewModel = new HomeViewModel()
+                {
+                    Posts = posts.ToList()
+                };
+                return View(homeViewModel);
+            }
             return View();
         }
         
