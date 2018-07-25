c2dbcaf05e27d54f2673756e3d4497839df457b3
Arosos <wierzba96@o2.pl>
2018-07-25 12:19:05 +0200

    HomeViewModel.

---
+++ SimpleWebApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using SimpleWebApp.Models;
+using System.Collections.Generic;
+
+namespace SimpleWebApp.ViewModels
+{
+    public class HomeViewModel
+    {
+        public Post Post { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+}
